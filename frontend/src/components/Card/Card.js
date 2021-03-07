6d37e8c78b5e37665d49743675c38db4b75f34ff
Moishe Barnes <moishebarnes@gmail.com>
2021-03-07 14:58:22 +0000

    created structure for cards

--- frontend/src/components/Card/Card.js
+++ frontend/src/components/Card/Card.js
@@ -3,7 +3,20 @@ import './Card'
 function Card(props) {
   return (
     <div className="card">
-      {props.country}
+      <div className="card-top">
+        <img src={props.country} alt={props.country}/>
+      </div>
+      <div className="card-bottom">
+        <h2 className='card-title'>{props.country}</h2>
+        <div>
+          <ul>
+            <li><span className='card-attribute'>Population: </span><p className='card-attribute-value'>{props.country}</p></li>
+            <li><span className='card-attribute'>Region: </span><p className='card-attribute-value'>{props.country}</p></li>
+            <li><span className='card-attribute'>Capital: </span><p className='card-attribute-value'>{props.country}</p></li>
+          </ul>
+        </div>
+      </div>
+      
     </div>
   );
 }
