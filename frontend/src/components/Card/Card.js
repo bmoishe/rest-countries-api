fc2b0ba40d9da6b1bab4ff1ed28565e54a8fec35
Moishe Barnes <moishebarnes@gmail.com>
2021-03-09 18:51:37 +0000

    [326505][Moishe]: fixing styling

--- frontend/src/components/Card/Card.js
+++ frontend/src/components/Card/Card.js
@@ -2,20 +2,19 @@ import './Card.css'
 
 function Card(props) {
     return (
-      <button className="card" onClick={() => props.setSelectedCountry(props.country)}>
+      <div className="card" onClick={() => props.setSelectedCountry(props.country)}>
         <div className="card-top">
           <img src={props.country.flag} alt={props.country.name}/>
         </div>
         <div className="card-bottom">
-          <h2 className='card-title'>{props.country.name}</h2>
+          <h3 className='card-title'>{props.country.name}</h3>
           <div>
               <div><span className='card-attribute'>Population: </span><span className='card-attribute-value'>{props.country.population}</span></div>
               <div><span className='card-attribute'>Region: </span><span className='card-attribute-value'>{props.country.region}</span></div>
               <div><span className='card-attribute'>Capital: </span><span className='card-attribute-value'>{props.country.capital}</span></div>
           </div>
         </div>
-        
-      </button>
+      </div>
     );
 }
 export default Card;
